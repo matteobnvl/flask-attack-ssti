2c3fe4dfb9953bf0a6ea4e4614014ac36b25158f
matteobnvl <matteobonneval19@gmail.com>
2023-12-07 16:03:48 +0100

    init project

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, render_template_string
+
+app = Flask(__name__)
+
+@app.route('/',  methods=['GET', 'POST'])
+def hello_geek():
+    user = ''
+    if request.args.get('name'):
+        user = request.args.get('name')
+        template = """ <p>Welcome, {}</p>""".format(user)
+        return render_template_string(template)
+    return render_template('index.html', context={'user': user})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
